4c318c8641f646dcf912d07b80ba1ba18a50835d
P7ori <pedrosartdr@gmail.com>
2022-05-12 16:15:22 -0300

    MainViewController

--- src/application/Main.java
+++ src/application/Main.java
@@ -6,31 +6,25 @@ import javafx.scene.Scene;
 import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
-public class Main extends Application
-{
+public class Main extends Application {
 	@Override
-	public void start(Stage stage) throws Exception 
-	{
-		try
-		{
+	public void start(Stage stage) throws Exception {
+		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
 			ScrollPane scrollPane = loader.load();
-			
+
 			scrollPane.setFitToHeight(true);
 			scrollPane.setFitToWidth(true);
-			
+
 			Scene scene = new Scene(scrollPane);
 			stage.setScene(scene);
 			stage.show();
-		}
-		catch(Exception ex)
-		{
+		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
 	}
 
-	public static void main(String[] args)
-	{
+	public static void main(String[] args) {
 		launch();
 	}
 }
--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -1,5 +1,28 @@
 package gui;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.MenuItem;
+
 public class MainViewController 
 {
+	//FIELDS________________________________________
+	@FXML private MenuItem menuItemSeller;
+	@FXML private MenuItem menuItemDepartment;
+	@FXML private MenuItem menuItemAbout;
+	
+	//EVENTS________________________________________
+	@FXML private void menuItemSeller_click()
+	{
+		System.out.println("menuItemSeller_Click()");
+	}
+	
+	@FXML private void menuItemDepartment_click()
+	{
+		System.out.println("menuItemDepartment_Click()");
+	}
+	
+	@FXML private void menuItemAbout_click()
+	{
+		System.out.println("menuItemAbout_Click()");
+	}
 }
